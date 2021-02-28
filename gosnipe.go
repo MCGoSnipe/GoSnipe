c1562ebf25c80c4492fd4ff066d76043dadfc731
overestimate <58041173+overestimate@users.noreply.github.com>
2021-02-27 18:56:25 -0600

    fix gosnipe being bad with pre-specified bearers also a small change that should help with recieving

--- gosnipe.go
+++ gosnipe.go
@@ -51,30 +51,36 @@ func initFlags() {
 
 }
 
-func timeSnipe(timestampTemp *time.Time, lines []string) {
-	timestamp := *timestampTemp
-	ch := make(chan gosnipe.SnipeRes)
+func timeSnipe(ch chan gosnipe.SnipeRes, timestamp time.Time, lines []string) {
 	time.Sleep(time.Until(timestamp.Add(time.Second * time.Duration(20))))
 	bearers, labels, _ := gosnipe.SliceStrToBearers(lines)
-	for i, bearer := range bearers {
-		config := gosnipe.Configuration{
-			Bearer:    bearer,
-			Name:      name,
-			Offset:    offset + float64(speedlimit*i),
-			Timestamp: timestamp,
-			Label:     &labels[i],
-		}
+	i := 0
+	if isFlagPassed("microsoft") {
+		i++
+	}
+	for _, bearer2 := range bearers {
 		for j := 0; j < snipereqs; j++ {
+			config := gosnipe.Configuration{
+				Bearer:    bearer2,
+				Name:      name,
+				Offset:    offset + float64(speedlimit*i*snipereqs+j),
+				Timestamp: timestamp,
+				Label:     &labels[i],
+			}
 			go gosnipe.Snipe(config, ch)
-			go getResp(ch)
 		}
+		i++
 	}
 }
 
 func getResp(ch chan gosnipe.SnipeRes) {
-	snipeRes := <-ch
-	if snipeRes.Status != nil {
-		fmt.Println(*snipeRes.Label + "Status: " + strconv.Itoa(*snipeRes.Status) + " Sent:" + snipeRes.Sent.Format(time.RFC3339) + " Recv: " + snipeRes.Recv.Format(time.RFC3339))
+	for true {
+		snipeRes := <-ch
+		if snipeRes.Status != nil {
+			fmt.Println(*snipeRes.Label + "Status: " + strconv.Itoa(*snipeRes.Status) + " Sent:" + snipeRes.Sent.Format(time.RFC3339) + " Recv: " + snipeRes.Recv.Format(time.RFC3339))
+		} else {
+			fmt.Println("Status was nil.")
+		}
 	}
 }
 
@@ -107,17 +113,24 @@ func main() {
 			}
 		}
 	}
-	bearers := make([]string, 0)
-	labels := make([]string, 0)
 	var lines []string
 	if isFlagPassed("path") {
 		lines, _ = gosnipe.TextToSliceStr(path)
 	}
+	timestampTemp := gosnipe.GetDropTime(name)
+	if timestampTemp == nil {
+		fmt.Println("Failed to fetch droptime.")
+		os.Exit(1)
+	}
+	timestamp := *timestampTemp
+	ch := make(chan gosnipe.SnipeRes)
+	go timeSnipe(ch, timestamp, lines)
 	if isFlagPassed("microsoft") {
 		if msa {
-			fmt.Println("Head to the link below, authorize the app, and paste the page shown afterwards here.")
-			fmt.Println(gosnipe.MicrosoftLoginAPI)
+			label := "Microsoft Account"
 			if !isFlagPassed("bearer") {
+				fmt.Println("Head to the link below, authorize the app, and paste the page shown afterwards here.")
+				fmt.Println(gosnipe.MicrosoftLoginAPI)
 				res, err := read.ReadString('\n')
 				if err != nil {
 					fmt.Println("Failed to read from STDIN.")
@@ -125,13 +138,25 @@ func main() {
 				}
 				var resp msaRes
 				json.Unmarshal([]byte(res), &resp)
-				bearers = append(bearers, *resp.AccessToken)
-				labels = append(labels, "Microsoft Account")
+				config := gosnipe.Configuration{
+					Bearer:    bearer,
+					Name:      name,
+					Offset:    offset + float64(speedlimit*0),
+					Timestamp: timestamp,
+					Label:     &label,
+				}
+				go gosnipe.Snipe(config, ch)
 			} else {
 				var resp msaRes
 				json.Unmarshal([]byte(bearer), &resp)
-				bearers = append(bearers, *resp.AccessToken)
-				labels = append(labels, "Microsoft Account")
+				config := gosnipe.Configuration{
+					Bearer:    bearer,
+					Name:      name,
+					Offset:    offset + float64(speedlimit*0),
+					Timestamp: timestamp,
+					Label:     &label,
+				}
+				go gosnipe.Snipe(config, ch)
 			}
 		} else {
 			if !isFlagPassed("path") {
@@ -140,12 +165,8 @@ func main() {
 			}
 		}
 	}
-	timestampTemp := gosnipe.GetDropTime(name)
-	if timestampTemp == nil {
-		fmt.Println("Failed to fetch droptime.")
-		os.Exit(1)
-	}
-	go timeSnipe(timestampTemp, lines)
-	fmt.Println("Snipe running. Press enter to stop.")
+	fmt.Println("Snipe running. Press enter to close.")
+	go getResp(ch)
 	read.ReadString('\n')
+	os.Exit(0)
 }
